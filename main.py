6f3872b15dd196a412491314c7287fb1e3f9c880
Markus Witt <feuerrot@feuerrot.org>
2017-07-24 16:49:33 +0200

    main.py: fix freeze on broken connection

--- main.py
+++ main.py
@@ -10,49 +10,52 @@ config = {
 
 getshares = '{"type":"get_shares","params":{"user":"feuerrot"}}'
 
-import logging
-logger = logging.getLogger('websockets')
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
+def proxy_tcp_to_ws(input):
+	print("PROXY TCP -> WS")
+	return input
+
+def proxy_ws_to_tcp(input):
+	print("PROXY WS -> TCP")
+	return input
 
 async def tcp_to_ws(reader, ws):
-	print("tcp_to_ws")
 	while True:
-		print("tcp_to_ws while true")
 		data = await reader.read(1024)
 		try:
-			data = data.decode('ascii').strip()
-			print("tcp_to_ws got data")
-			if not data:
-				print("tcp_to_ws no data")
+			if data:
+				data = data.decode('ascii').strip()
 			else:
-				await ws.send(data)
-				print("tcp_to_ws sent data")
+				return
+			if data:
+				await ws.send(proxy_tcp_to_ws(data))
 		except Exception as e:
 			print(e)
+			return
 
 async def ws_to_tcp(writer, ws):
-	print("ws_to_tcp")
 	while True:
-		print("ws_to_tcp while true")
 		data = await ws.recv()
-		print("ws_to_tcp got data")
-		writer.write(data.encode('ascii'))
-		print("ws_to_tcp wrote data")
-		await writer.drain()
-		print("ws_to_tcp writer drain")
+		try:
+			if data:
+				data = proxy_ws_to_tcp(data)
+				data = data.encode('ascii')
+				writer.write(data)
+				await writer.drain()
+			else:
+				return
+		except Exception as e:
+			print(e)
+			return
 
 async def accept_client(client_reader, client_writer):
-	print("accepted client")
+	print("accepted client: {}".format(client_writer.get_extra_info('peername')))
 	ws = await websockets.connect("{}:{}".format(config['ws_server'], config['ws_port']))
-	print("ws connected")
 	a = asyncio.ensure_future(tcp_to_ws(client_reader, ws))
 	b = asyncio.ensure_future(ws_to_tcp(client_writer, ws))
-	return (a,b)
 
 def handle_client(client_reader, client_writer):
 	print("got client")
-	task = asyncio.ensure_future(accept_client(client_reader, client_writer))
+	asyncio.ensure_future(accept_client(client_reader, client_writer))
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
